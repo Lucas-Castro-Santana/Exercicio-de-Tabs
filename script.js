2602a64770be36ad737b3ec5c170ace602b5fcab
Lucas-Castro-Santana <lucascastrosantana5@gmail.com>
2024-05-03 15:07:49 +0100

    adição da versão mobile

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-const tabs = document.querySelectorAll('.tab-btn');
-
-tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
-
-const tabClicked = (tab) => {
-    tabs.forEach(tab => tab.classList.remove('active'));
-    tab.classList.add('active');
-
-    const contents = document.querySelectorAll('.content');
-
-    contents.forEach(content => content.classList.remove('show'));
-
-    const contentId = tab.getAttribute('content-id');
-    const content = document.getElementById(contentId);
-
-    content.classList.add('show');
-}
-
-const currentActiveTab = document.querySelector('.tab-btn.active');
-tabClicked(currentActiveTab);
+$(document).ready(function() {
+    $('.content').hide();
+ 
+    $('.tab-btn').click(function() {
+        var tab = $(this);
+ 
+        $('.tab-btn').removeClass('active');
+        tab.addClass('active');
+ 
+        $('.content').hide();
+ 
+        var contentId = tab.attr('content-id');
+        $('#' + contentId).show();
+    });
+ 
+    var currentActiveTab = $('.tab-btn.active');
+    currentActiveTab.click();
+});
